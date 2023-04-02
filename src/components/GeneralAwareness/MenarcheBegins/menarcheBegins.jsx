fd4c4da132eae67ec4284d5a3908c10f82f00a17
raghuttam1508 <raghu.parvatikar@gmail.com>
2023-04-02 13:09:45 +0530

    updates

--- src/components/GeneralAwareness/MenarcheBegins/menarcheBegins.jsx
+++ src/components/GeneralAwareness/MenarcheBegins/menarcheBegins.jsx
@@ -13,48 +13,54 @@ export default function MenarcheBegins() {
         darlings have come forward to share their not-so-fun yet unforgettable
         experiences.
       </div>
-
-      <h4>A not-so-deadly experience:</h4>
-      <div>
-        So it was february or march of 2007 and I was attending school like
-        every other day. I was feeling something strange between my legs. It was
-        ofcourse unusual but I considered to ignore it. Fortunately, we had dark
-        blue color winter skirt so the stain did not show up. I reached home,
-        ate lunch and when I went to change my clothes, what I saw was
-        completely shocking and unbelievable. I came to realize from where I was
-        bleeding and that was the reason of feeling so unusual that day. I was
-        unable to fathom the situation. I started crying because I thought I was
-        going to die. Yes so this was the thought that struck me first. I kept
-        crying because I thought this was the end of my life. My whole world
-        fell apart by seeing a lot of bood on my thighs.
-        <br />I opened the door of my room slightly and called out my mom. I was
-        sobbing and explained what was happening to me. She stood there
-        emotionless, then brought pad and explained to me how to wear it.
+      <br />
+      <h4>--- A Not-So-Deadly Experience ---</h4>
+      <div className="text">
+        <p>
+          So it was february or march of 2007 and I was attending school like
+          every other day. I was feeling something strange between my legs. It
+          was ofcourse unusual but I considered to ignore it. Fortunately, we
+          had dark blue color winter skirt so the stain did not show up. I
+          reached home, ate lunch and when I went to change my clothes, what I
+          saw was completely shocking and unbelievable. I came to realize from
+          where I was bleeding and that was the reason of feeling so unusual
+          that day. I was unable to fathom the situation. I started crying
+          because I thought I was going to die. Yes so this was the thought that
+          struck me first. I kept crying because I thought this was the end of
+          my life. My whole world fell apart by seeing a lot of bood on my
+          thighs.I opened the door of my room slightly and called out my mom. I
+          was sobbing and explained what was happening to me. She stood there
+          emotionless, then brought pad and explained to me how to wear it.
+        </p>
       </div>
 
-      <h4>Proud to bleed:</h4>
-      <div>
-        When I was seven we were on holiday with my mom’s best friend and her
-        teenage daughter. I saw her pads and wanted to know what they were, so
-        several books were bought and I was well prepared.
-        <br />
-        Fast forward two years and while on holiday with a friend and her family
-        I stumble into the bathroom, half-asleep, at 6 a.m. Cue toilet paper,
-        dark color, exhausted confusion — and then jubilation! I was a woman! I
-        ran through the house hooting, got the mom to call my parents and was
-        proudly unable to swim that day.
+      <h4>--- Proud to Bleed ---</h4>
+      <div className="text">
+        <p>
+          When I was seven we were on holiday with my mom’s best friend and her
+          teenage daughter. I saw her pads and wanted to know what they were, so
+          several books were bought and I was well prepared. Fast forward two
+          years and while on holiday with a friend and her family I stumble into
+          the bathroom, half-asleep, at 6 a.m. Cue toilet paper, dark color,
+          exhausted confusion — and then jubilation! I was a woman! I ran
+          through the house hooting, got the mom to call my parents and was
+          proudly unable to swim that day.
+        </p>
       </div>
 
-      <h4>Fearsome Fourteen:</h4>
-      <div>
-        It was the 2nd day of final exams of 7th grade. I was casually sitting
-        on the bed when I had my eyes stuck on the huge patch of blood stain on
-        the bedsheet. Crying, I called my mom who couldn’t control her laughter
-        seeing my reaction to Lady Red. We are pretty understanding about the
-        situation and helped me throughout the 14 days of pain and bleeding.
+      <h4>--- Fearsome Fourteen ---</h4>
+      <div className="text">
+        <p>
+          It was the 2nd day of final exams of 7th grade. I was casually sitting
+          on the bed when I had my eyes stuck on the huge patch of blood stain
+          on the bedsheet. Crying, I called my mom who couldn’t control her
+          laughter seeing my reaction to Lady Red. We are pretty understanding
+          about the situation and helped me throughout the 14 days of pain and
+          bleeding.
+        </p>
       </div>
 
-      <div>
+      <div className="subtext">
         See, all the experiences are unique and unforgettable, but none are
         painful or embarrassing. Own the flow ladies!
       </div>
