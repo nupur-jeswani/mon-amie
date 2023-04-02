b945865de08f2e07d2025c95a3e0209f6c0d752e
raghuttam1508 <raghu.parvatikar@gmail.com>
2023-04-02 12:06:07 +0530

    CSS and PCOS

--- src/components/GeneralAwareness/RedBadgeOfCourage/redBadge.jsx
+++ src/components/GeneralAwareness/RedBadgeOfCourage/redBadge.jsx
@@ -1,53 +1,92 @@
-import React from 'react';
-import "../styling.css"
+import React from "react";
+import "./redBadge.css";
 
 export default function RedBadge() {
-    return (
-        <div className='container-flex' style={{ backgroundColor: "#f5f5ef" }}>
-            <div className="row">
-                <div className="col-md-3 indexing">
-                    Go to -
-                    <br />
-                    - What is menstruation/periods?
-                </div>
-                <div className="col-md-9">
-                    <h1 className='heading1'>What is menstruation/periods - </h1>
-                    <div className='text'>
-                        Menstruation is when the lining of the uterus sheds, breaking down into blood. This blood then leaves the body through the vagina. It is also known as a period.
-                        <br />
-                        Periods usually last around 3–7 daysTrusted Source. During this time, a person who is menstruating bleeds from the vagina. They may also experience other symptoms, such as cramping.
-                        <br />
-                        Menstruation stops when someone is pregnant, so having a period means a person is not pregnant. It also marks the beginning of a new menstrual cycle. This cycle of fertility affects most females from their teenage years through to menopause when periods stop.
-                        <br />
-                        Periods happen due to the menstrual cycle. This is the rise and fall of hormones, such as estrogen and progesterone, that influence female fertility over the course of around 21–35 daysTrusted Source.
-                        <br />
-                        Fertility peaks roughly halfway through the cycle with ovulation, which is when the ovaries release an egg. This causes the uterine lining to thicken in preparation for a potential pregnancy. If no sperm fertilizes the egg, the uterus sheds this lining, and the cycle begins again.
-                        <br />
-                        The length of someone’s cycle, the duration of their periods, and the day they ovulate vary from person to person. Sometimes, it also differs from cycle to cycle.
-
-                    </div>
-
-                    <h4 className='heading1'>When do periods start </h4>
-                    <div className='text'>
-                        A person’s first period indicates that the menstrual cycle has begun. Doctors refer to this as menarche. For most females, it occurs between the ages of 9–15Trusted Source, with the average age being 12.4 years.
-                        <br />
-                        A period is one of the signs of puberty, which is the beginning of a journey toward sexual maturity. This likely means that it is possible to get pregnant.
-                        <br />
-                        However, a person can become pregnant from the first time they ovulate, which means it is possible to get pregnant before the first period.
-                        <br />
-                        Other Signs of puberty include -
-                        <ol>
-                            <li>growth spurts</li>
-                            <li>increased fat on the hips, thighs, and buttocks</li>
-                            <li>developing more body hair, especially on the legs and under the arms</li>
-                            <li>growing pubic hair</li>
-                        </ol>
-                        Most young people develop breasts about 2.5 years before menarche. If a person does not get their first period within 5 years of breast development, doctors consider this delayed menarche
-                    </div>
-                </div>
-            </div>
-
+  return (
+    <div class="box">
+      {/* <div className="container-flex" style={{ backgroundColor: "#f5f5ef" }}> */}
+      <div className="row" style={{ backgroundColor: "#f5f5ef" }}>
+        {/* <div className="col-md-3 indexing">
+          Go to -
+          <br />- What is menstruation/periods?
+        </div> */}
+        {/* <div className="col-md-9"> */}
+        <h1 className="heading1">What is menstruation/periods? </h1>
+        <div className="text">
+          <li>
+            Menstruation is when the lining of the uterus sheds, breaking down
+            into blood. This blood then leaves the body through the vagina. It
+            is also known as a period.
+          </li>
+          <li>
+            Periods usually last around 3–7 daysTrusted Source. During this
+            time, a person who is menstruating bleeds from the vagina. They may
+            also experience other symptoms, such as cramping.
+          </li>
+          <li>
+            Menstruation stops when someone is pregnant, so having a period
+            means a person is not pregnant. It also marks the beginning of a new
+            menstrual cycle. This cycle of fertility affects most females from
+            their teenage years through to menopause when periods stop.
+          </li>
+          <li>
+            Periods happen due to the menstrual cycle. This is the rise and fall
+            of hormones, such as estrogen and progesterone, that influence
+            female fertility over the course of around 21–35 daysTrusted Source.
+          </li>
+          <li>
+            Fertility peaks roughly halfway through the cycle with ovulation,
+            which is when the ovaries release an egg. This causes the uterine
+            lining to thicken in preparation for a potential pregnancy. If no
+            sperm fertilizes the egg, the uterus sheds this lining, and the
+            cycle begins again.
+          </li>
+          <li>
+            The length of someone’s cycle, the duration of their periods, and
+            the day they ovulate vary from person to person. Sometimes, it also
+            differs from cycle to cycle.
+          </li>
         </div>
-    )
-}
 
+        <h1 className="heading1">When do periods start? </h1>
+        <div className="text">
+          <li>
+            A person’s first period indicates that the menstrual cycle has
+            begun. Doctors refer to this as menarche. For most females, it
+            occurs between the ages of 9–15 Trusted Source, with the average age
+            being 12.4 years.
+          </li>
+
+          <li>
+            A period is one of the signs of puberty, which is the beginning of a
+            journey toward sexual maturity. This likely means that it is
+            possible to get pregnant.
+          </li>
+          <li>
+            However, a person can become pregnant from the first time they
+            ovulate, which means it is possible to get pregnant before the first
+            period.
+            <li>Other Signs of puberty include -</li>
+            <ol>
+              <li>
+                <li>Growth spurts</li>
+                <li>Increased fat on the hips, thighs, and buttocks</li>
+                <li>
+                  Developing more body hair, especially on the legs and under
+                  the arms
+                </li>
+                <li>Growing pubic hair</li>
+              </li>
+            </ol>
+            <li>
+              Most young people develop breasts about 2.5 years before menarche.
+              If a person does not get their first period within 5 years of
+              breast development, doctors consider this delayed menarche
+            </li>
+          </li>
+        </div>
+      </div>
+    </div>
+    // </div>
+  );
+}
--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,66 +1,80 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useNavigate } from 'react-router-dom';
-import React, { useState } from 'react';
-import logo from './Images/logo.png';
+import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import logo from "./Images/logo.png";
 import NavbarToggle from "react-bootstrap/esm/NavbarToggle";
 import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
-import { Alert, Nav, Navbar, Button, NavLink, NavbarBrand } from "react-bootstrap";
+import {
+  Alert,
+  Nav,
+  Navbar,
+  Button,
+  NavLink,
+  NavbarBrand,
+} from "react-bootstrap";
 import { UserAuth } from "../../context/AuthContext";
 
-
 export default function NavBar() {
+  // const user = UserAuth();
+  // const logout = UserAuth();
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const { user, logout } = UserAuth();
 
-    // const user = UserAuth();
-    // const logout = UserAuth();
-    const [error, setError] = useState("");
-    const navigate = useNavigate();
-    const { user, logout } = UserAuth();
+  async function handleLogOut() {
+    setError("");
 
-    async function handleLogOut() {
-        setError("");
-
-        try {
-            await logout();
-            navigate('/landing')
-        } catch(e) {
-            setError("Failed to logout of your account!", e);
-        }
+    try {
+      await logout();
+      navigate("/landing");
+    } catch (e) {
+      setError("Failed to logout of your account!", e);
     }
+  }
 
-
-    return (
-        <>
-            <Navbar bg="light" expand="lg" className="container-fluid  rounded-bottom shadow-sm" sticky="top">
-                <NavbarBrand className="ms-3"><img className="ms-2" src={logo} alt="monamie logo" width="10%"/></NavbarBrand>
-                <NavbarToggle aria-controls="basic-navbar-nav" />
-                <NavbarCollapse id="basic-navbar-nav">
-                    <Nav className="container-fluid justify-content-end fw-bold me-auto">
-                        { user ?
-                            <>
-                                {/* <NavbarBrand className="ms-auto ms-3" href="/home"><img classname="navlogo" src={logo} alt="monamie logo" width="10%"/></NavbarBrand> */}
-                                <NavLink href="/home">Home</NavLink>
-                                {/* <NavLink href="/pcos">PCOS Prediction</NavLink> */}
-                                {/* <NavLink href="/home">Cycle Tracker</NavLink> */}
-                                <NavLink href="/awareness">Periods Awareness</NavLink>
-                                <NavLink href="/productLanding">Periods Products</NavLink>
-                                <NavLink href="/doctors">Doctor Consultancy</NavLink>
-                                <NavLink href="/fitness">Fitness / Exercises</NavLink>
-                                <Button className="btn btn-dark fw-bold" onClick={handleLogOut}>LOGOUT</Button>
-                            </>
-                            :
-                            <>
-                                {/* <NavbarBrand className="" href="/landing"><img classname="navlogo" src={logo} alt="monamie logo" width="10%"/></NavbarBrand> */}
-                                <NavLink href="/login">Login</NavLink>
-                                <NavLink href="/signup">Create Account</NavLink>
-                                <NavLink href="/landing">Home</NavLink>
-                                <NavLink href="/awareness">Periods Awareness</NavLink>
-                                <NavLink href="/productLanding">Periods Products</NavLink>
-                            </>
-                        }
-                    </Nav>
-                </NavbarCollapse>
-            </Navbar>
-            {error && <Alert variant="danger"> {error} </Alert>}
-        </>
-    )
+  return (
+    <>
+      <Navbar
+        bg="light"
+        expand="lg"
+        className="container-fluid  rounded-bottom shadow-sm"
+        sticky="top"
+      >
+        <NavbarBrand className="ms-3">
+          <img className="ms-2" src={logo} alt="monamie logo" width="10%" />
+        </NavbarBrand>
+        <NavbarToggle aria-controls="basic-navbar-nav" />
+        <NavbarCollapse id="basic-navbar-nav">
+          <Nav className="container-fluid justify-content-end fw-bold me-auto">
+            {user ? (
+              <>
+                {/* <NavbarBrand className="ms-auto ms-3" href="/home"><img classname="navlogo" src={logo} alt="monamie logo" width="10%"/></NavbarBrand> */}
+                <NavLink href="/home">Home</NavLink>
+                <NavLink href="http://localhost:8501/">All about PCOS</NavLink>
+                {/* <NavLink href="/pcos">PCOS Prediction</NavLink> */}
+                {/* <NavLink href="/home">Cycle Tracker</NavLink> */}
+                <NavLink href="/awareness">Periods Awareness</NavLink>
+                <NavLink href="/productLanding">Periods Products</NavLink>
+                <NavLink href="/doctors">Doctor Consultancy</NavLink>
+                <NavLink href="/fitness">Fitness / Exercises</NavLink>
+                <Button className="btn btn-dark fw-bold" onClick={handleLogOut}>
+                  LOGOUT
+                </Button>
+              </>
+            ) : (
+              <>
+                {/* <NavbarBrand className="" href="/landing"><img classname="navlogo" src={logo} alt="monamie logo" width="10%"/></NavbarBrand> */}
+                <NavLink href="/login">Login</NavLink>
+                <NavLink href="/signup">Create Account</NavLink>
+                <NavLink href="/landing">Home</NavLink>
+                <NavLink href="/awareness">Periods Awareness</NavLink>
+                <NavLink href="/productLanding">Periods Products</NavLink>
+              </>
+            )}
+          </Nav>
+        </NavbarCollapse>
+      </Navbar>
+      {error && <Alert variant="danger"> {error} </Alert>}
+    </>
+  );
 }
