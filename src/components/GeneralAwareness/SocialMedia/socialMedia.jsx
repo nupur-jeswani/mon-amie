85aabe105e64f4de21c21434db9fc7a7ffc1b587
raghuttam1508 <raghu.parvatikar@gmail.com>
2023-04-05 20:25:36 +0530

    css updations

--- src/components/GeneralAwareness/SocialMedia/socialMedia.jsx
+++ src/components/GeneralAwareness/SocialMedia/socialMedia.jsx
@@ -1,42 +1,95 @@
-import React from 'react'
+import React from "react";
+import "./socialMedia.css";
 
 export default function SocialMedia() {
-    return (
-        <div>
-            <h1>Social Media made it easy</h1>
-            <div>
-                Hey lil’ birdies! Social media is our go-to for everything. Be it recipes, gym workout plans, advises etc. It’s reach can change the course of society.
-                <br />
-                Let’s dive into some of the content creators and their contents based on what you like!
-            </div>
+  return (
+    <div>
+      <h1 className="heading1">Social Media made it Easy</h1>
+      <div className="subtext">
+        Hey lil’ birdies! Social media is our go-to for everything. Be it
+        recipes, gym workout plans, advises etc. It’s reach can change the
+        course of society.
+        <br />
+        Let’s dive into some of the content creators and their contents based on
+        what you like!
+      </div>
 
-            <h4>Dr. Cuterus:</h4>
-            <div>
-                Tanaya Narendra, better known as Dr Cuterus on Instagram, offers bite-sized information on sexual health, one post at a time. An oxford trained medical professional and firm believer in the importance of sexual well-being for overall health, she talks about everything from period pain, PCOS/PCOD, sex, reproductive health for both men and women, menstrual health, hysterectomy, among others. At the peak of COVID-19, she used her social media presence with over 500,000 followers to share emergency needs including medical beds and oxygen, and connected them with the right resources. The influencer is also vocal about body positivity, and calls out body and fat shaming instances to sensitise the public on their social interactions.
-                <br />
-                <a href="https://www.youtube.com/watch?v=uzR70T4fnFY" target={"_blank"}>Check out her Youtube channel for absolutely relatable content: </a>
-                <br />
-                <a href="https://www.instagram.com/dr_cuterus/?hl=en" target={"_blank"}>Check out her Instagram account for amazingly short but clear reels on Women’s health</a>
-            </div>
+      <h4>Dr. Cuterus:</h4>
+      <div>
+        Tanaya Narendra, better known as Dr Cuterus on Instagram, offers
+        bite-sized information on sexual health, one post at a time. An oxford
+        trained medical professional and firm believer in the importance of
+        sexual well-being for overall health, she talks about everything from
+        period pain, PCOS/PCOD, sex, reproductive health for both men and women,
+        menstrual health, hysterectomy, among others. At the peak of COVID-19,
+        she used her social media presence with over 500,000 followers to share
+        emergency needs including medical beds and oxygen, and connected them
+        with the right resources. The influencer is also vocal about body
+        positivity, and calls out body and fat shaming instances to sensitise
+        the public on their social interactions.
+        <br />
+        <a href="https://www.youtube.com/watch?v=uzR70T4fnFY" target={"_blank"}>
+          Check out her Youtube channel for absolutely relatable content:{" "}
+        </a>
+        <br />
+        <a href="https://www.instagram.com/dr_cuterus/?hl=en" target={"_blank"}>
+          Check out her Instagram account for amazingly short but clear reels on
+          Women’s health
+        </a>
+      </div>
 
-            <h4>Dr. Riddhima Shetty:</h4>
-            <div>
-                Dr Riddhima Shetty is an obstetrician - gynaecologist who believes in a modern, judgement-free, holistic approach to women's health.
-                <br />
-                She graduated with the 1st rank at her post-graduate residency program and continued to gain experience in laparoscopic surgery and ultrasound to hone her skills.
-                She has a demonstrated history of working in the hospital & health care industry.
-                She started the social media profile known as @friendly_neighbourhood_OBGYN on instagram where she shares her knowledge and information on women's health for the public.
-                She is highly skilled in obstetric & gynaecological surgery, healthcare, clinical research, teaching and also has a flair for art such as writing, public speaking and dance.
-                <br />
-                <a href="https://www.instagram.com/friendly_neighbourhood_obgyn/?hl=en" target={"_blank"}>Check out her Instagram account for amazingly short but clear reels on Women’s health</a>
-            </div>
+      <h4>Dr. Riddhima Shetty:</h4>
+      <div>
+        Dr Riddhima Shetty is an obstetrician - gynaecologist who believes in a
+        modern, judgement-free, holistic approach to women's health.
+        <br />
+        She graduated with the 1st rank at her post-graduate residency program
+        and continued to gain experience in laparoscopic surgery and ultrasound
+        to hone her skills. She has a demonstrated history of working in the
+        hospital & health care industry. She started the social media profile
+        known as @friendly_neighbourhood_OBGYN on instagram where she shares her
+        knowledge and information on women's health for the public. She is
+        highly skilled in obstetric & gynaecological surgery, healthcare,
+        clinical research, teaching and also has a flair for art such as
+        writing, public speaking and dance.
+        <br />
+        <a
+          href="https://www.instagram.com/friendly_neighbourhood_obgyn/?hl=en"
+          target={"_blank"}
+        >
+          Check out her Instagram account for amazingly short but clear reels on
+          Women’s health
+        </a>
+      </div>
 
-            <h4>Dr. Aditi Tandon:</h4>
-            <div>
-                She has completed her MBBS from K. J. Somaiya Medical College, Mumbai, and MS ( OBSTETRICS AND GYNAECOLOGY) from Rural Medical College, Loni. She takes keen interest in Infertility Treatment, IVF and Gynaec Endoscopy. She is attached to DY Patil hospital as an Assistant Professor. She has completed Masters in Reproductive medicine and IVF from the prestigious Homerton Univerity, London, and has done a fellowship in Advance minimal access surgery and ultrasound. She has an experience of 6 years in the field of Obstetrics and Gynecology. She is well trained and efficient in managing all cases of infertility and IVF. She is greatly skilled in performed major laparoscopic surgeries including TLH, myomectomy for fibroids, endometriosis surgery and ovarian cystectomy. She is well experienced in performing hysteroscopic surgeries as well. She has one national and one international publications in indexed journals and has presented one poster and paper at national conferences. She has won many awards for best paper presentation in various national and international conferences. She is a great asset to our team.
-                <br />
-                <a href="https://www.instagram.com/the_fertilitydoc/?igshid=YmMyMTA2M2Y%3D" target={"_blank"}>Check out her Instagram account for amazingly short but clear reels on Women’s health</a>
-            </div>
-        </div>
-    )
+      <h4>Dr. Aditi Tandon:</h4>
+      <div>
+        She has completed her MBBS from K. J. Somaiya Medical College, Mumbai,
+        and MS ( OBSTETRICS AND GYNAECOLOGY) from Rural Medical College, Loni.
+        She takes keen interest in Infertility Treatment, IVF and Gynaec
+        Endoscopy. She is attached to DY Patil hospital as an Assistant
+        Professor. She has completed Masters in Reproductive medicine and IVF
+        from the prestigious Homerton Univerity, London, and has done a
+        fellowship in Advance minimal access surgery and ultrasound. She has an
+        experience of 6 years in the field of Obstetrics and Gynecology. She is
+        well trained and efficient in managing all cases of infertility and IVF.
+        She is greatly skilled in performed major laparoscopic surgeries
+        including TLH, myomectomy for fibroids, endometriosis surgery and
+        ovarian cystectomy. She is well experienced in performing hysteroscopic
+        surgeries as well. She has one national and one international
+        publications in indexed journals and has presented one poster and paper
+        at national conferences. She has won many awards for best paper
+        presentation in various national and international conferences. She is a
+        great asset to our team.
+        <br />
+        <a
+          href="https://www.instagram.com/the_fertilitydoc/?igshid=YmMyMTA2M2Y%3D"
+          target={"_blank"}
+        >
+          Check out her Instagram account for amazingly short but clear reels on
+          Women’s health
+        </a>
+      </div>
+    </div>
+  );
 }
