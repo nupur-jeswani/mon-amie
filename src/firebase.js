a4e688e14b05ee30276eb0b4846955c909026454
raghuttam1508 <raghu.parvatikar@gmail.com>
2023-04-05 20:30:17 +0530

    updates

--- src/firebase.js
+++ src/firebase.js
@@ -9,16 +9,16 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDNlQa794zfmDm-r8bDO5ZUSWUSCQhsmLY",
-    authDomain: "monamie-9cd2c.firebaseapp.com",
-    projectId: "monamie-9cd2c",
-    storageBucket: "monamie-9cd2c.appspot.com",
-    messagingSenderId: "343315018225",
-    appId: "1:343315018225:web:46078cf2e0b68104289450"
+  apiKey: "AIzaSyDNlQa794zfmDm-r8bDO5ZUSWUSCQhsmLY",
+  authDomain: "monamie-9cd2c.firebaseapp.com",
+  projectId: "monamie-9cd2c",
+  storageBucket: "monamie-9cd2c.appspot.com",
+  messagingSenderId: "343315018225",
+  appId: "1:343315018225:web:46078cf2e0b68104289450",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore();
-export default app
+export default app;
